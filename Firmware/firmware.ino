c96837412c66b3b8019cdcb574632e6e36028f68
Mikhail Prut <prutmihail@gmail.com>
2015-04-18 12:37:04 +0300

    Firmware
    Added blank for colebration function

--- Firmware/firmware.ino
+++ Firmware/firmware.ino
@@ -1,3 +1,19 @@
+/*
+Small CNC with ARDUINO controller
+Version: 0.0.1a
+
+*/
+int LastError; // To save error code
+boolean Colebration()
+/*
+Function which do a colibration a CNC
+Return TRUE if all is OK
+Return FALSE if has any problem. In this case variable LastError has a code of error
+
+*/
+{
+  return true;
+}
 void setup() {
   // put your setup code here, to run once:
 
